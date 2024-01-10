25a9659e81743eb3f0e3d49849d04ee089219403
LasseLuehmann <waffle669@yahoo.de>
2024-01-10 14:12:57 +0100

    added function

--- assets/js/form.js
+++ assets/js/form.js
@@ -11,7 +11,7 @@ function validation() {
         return false 
     }
 
-    if (firstName.length < 8) {
+    if (firstName.length < 5) {
         alert ("the name should be atleast 8 charecters long") 
         return false 
     }
@@ -24,4 +24,32 @@ function validation() {
     if (country == "usa" && gender != "female") {
         alert ("We only register females in USA")
     }
+}
+function activateSubmit() {
+
+    const firstName = document.registration.first_name.value
+    const lastName = document.registration.last_name.value
+    const email = document.registration.email.value
+    const submitBtn = document.getElementById('submit')
+
+    if (email.length > 0 && firstName.length > 0 && lastName.length > 0) {
+        // remove 'submit-disabled' class from submit button
+        submitBtn.classList.remove('submit-disabled')
+    } else {
+        // add 'submit-disabled' class if it doesn't exist
+        submitBtn.classList.add('submit-disabled')
+    }
+}
+function countText() {
+    const textareaCnt = document.registration.message.value.length
+    const para = document.getElementById('text-count')
+
+    if (textareaCnt == 100) {
+        // change the text color to green
+        para.style.color = '#020'
+    } else {
+        // change the text color to red
+        para.style.color = '#900'
+    }
+    para.textContent = textareaCnt + "/100"
 }
